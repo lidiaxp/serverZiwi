2279acc213071f4313ccb4c899acb4ca16abf034
Lidia Gianne <lidiagianne@gmail.com>
2019-01-30 16:49:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1185,7 +1185,7 @@ def coberturaUnity():
 	perda = []
 	perdas = []
 	perda_f = []
-	
+	'''
 	if modelo == 'mk':
 		ph = []
 		pv = []
@@ -1203,7 +1203,7 @@ def coberturaUnity():
         			p1 = [[float(conteudo.split(" ")[1]), float(conteudo.split(" ")[2])], [float(conteudo.split(" ")[3]), float(conteudo.split(" ")[4])]]
         			pv.append(p1)
         			modelv.append(int(conteudo.split(" ")[5]))
-    
+    	'''
 	ph = [[[0.0, 6.0], [4.0, 4.0]], [[0.0, 6.0], [5.0, 5.0]]]
 	pv = [[[1.5, 1.5], [0.0, 4.0]]]
 	modelh = [1, 2]
